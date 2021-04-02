ff3d9e95afcb1735ab101f81cb2a4da3cb83cf9c
Toxe <toxe@toxe.net>
2021-04-02 14:56:41 +0200

    Add sfml_60fps.cpp

---
+++ src/sfml/sfml_60fps.cpp
@@ -0,0 +1,29 @@
+#include <fmt/core.h>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML");
+    window.setFramerateLimit(60);
+
+    sf::Clock clock;
+
+    while (window.isOpen()) {
+        sf::Event event;
+
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        auto elapsedTime = clock.restart();
+        auto fps = 1.0f / elapsedTime.asSeconds();
+        auto size = window.getSize();
+        window.setTitle(fmt::format("SFML {}x{} FPS: {:.0f}", size.x, size.y, fps));
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
+    return 0;
+}
